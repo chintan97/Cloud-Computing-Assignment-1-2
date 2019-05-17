727db93aee984c9c6530d2150ef468178f55fe17
Chintan Patel <chintan97patel@gmail.com>
2019-05-16 22:15:05 -0300

    API created

---
+++ bmi/api/controllers/BmiController.js
@@ -0,0 +1,12 @@
+/**
+ * BmiController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
