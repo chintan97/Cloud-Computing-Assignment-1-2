d8cb44446b1994d61c8f7bfba1b6ed5228aa98c8
Chintan Patel <chintan97patel@gmail.com>
2019-05-21 14:38:43 -0300

    Error handling added
    Some error handling added.
    - If server is not available.
    - If request is wrongly sent.

--- bmi/api/controllers/BmiController.js
+++ bmi/api/controllers/BmiController.js
@@ -7,11 +7,16 @@
 
 module.exports = {
     calculate089: function(req, res){
-      var weight089 = req.body.weight089;   // parse weight value
-      var height089 = req.body.height089;   // parse height value
-      var bmi089 = weight089/ (height089 * height089);   // Calculate BMI = (weight) / (height*height)
-      return res.ok(bmi089);   // Send calculated BMI value in response
-  }
+        if (req.body.weight089 && req.body.height089){
+            var weight089 = req.body.weight089;   // parse weight value
+            var height089 = req.body.height089;   // parse height value
+            var bmi089 = weight089/ (height089 * height089);   // Calculate BMI = (weight) / (height*height)
+            return res.ok(bmi089);   // Send calculated BMI value in response
+        }
+        else{
+            res.status(500).send('Parameters are not valid!');
+        }
+    }
 
 };
 
