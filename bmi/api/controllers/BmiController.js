963c6b46a088b1e7ea4b58aca3b809d673180aa3
Chintan Patel <chintan97patel@gmail.com>
2019-05-21 11:32:41 -0300

    Web service complete
    index.html will call web service which is running on 1337 port.

--- bmi/api/controllers/BmiController.js
+++ bmi/api/controllers/BmiController.js
@@ -7,12 +7,10 @@
 
 module.exports = {
     calculate089: function(req, res){
-      var weight089 = req.body.weight089;
-      var height089 = req.body.height089;
-
-      var bmi089 = weight089/ (height089 * height089); 
-
-      res.view('pages/homepage', {bmi089: bmi089, weight089: weight089, height089: height089});
+      var weight089 = req.body.weight089;   // parse weight value
+      var height089 = req.body.height089;   // parse height value
+      var bmi089 = weight089/ (height089 * height089);   // Calculate BMI = (weight) / (height*height)
+      return res.ok(bmi089);   // Send calculated BMI value in response
   }
 
 };
