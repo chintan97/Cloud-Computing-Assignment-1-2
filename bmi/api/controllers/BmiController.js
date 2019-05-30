fed98c2dbaac01547a0fe8e7ba70185385c8b986
Chintan Patel <chintan97patel@gmail.com>
2019-05-30 11:30:38 -0300

    ASSINGMENT 2 complete
    Assignment 2 completed. index.html file contains front end. bmi_front089 contains front-end app as required in assignment 2

--- bmi/api/controllers/BmiController.js
+++ bmi/api/controllers/BmiController.js
@@ -10,11 +10,23 @@ module.exports = {
         if (req.body.weight089 && req.body.height089){
             var weight089 = req.body.weight089;   // parse weight value
             var height089 = req.body.height089;   // parse height value
-            var bmi089 = weight089/ (height089 * height089);   // Calculate BMI = (weight) / (height*height)
+            var bmi089;
+            if (weight089 <= 0){   // Weight can not be less than or equal to zero
+                bmi089 = "Weight not valid!";  
+            }
+            else if (height089 <= 0){   // Height can not be less than or equal to zero
+                bmi089 = "Height not valid!";
+            }
+            else if (weight089 > 0 && height089 > 0){
+                bmi089 = weight089/ (height089 * height089);   // Calculate BMI = (weight) / (height*height)
+            }
+            else{   // Any other cases like if string is passed
+                bmi089 = "Values not valid!";
+            }
             return res.ok(bmi089);   // Send calculated BMI value in response
         }
         else{
-            res.status(500).send('Parameters are not valid!');
+            return res.ok("Something wrong with JSON parameters!");
         }
     }
 
